d67542811361134ae664bac09a37313ec62806ac
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-06-25 13:01:42 +0200

    Add class Movie with construct and function to get title

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+/* 
+- è definita una classe ‘Movie’
+=> all'interno della classe sono dichiarate delle variabili d'istanza
+=> all'interno della classe è definito un costruttore
+=> all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+*/
+
+/**
+ * @author Riccardo <email@email.com>
+ * @copyright 2021 Riccardo
+ */
+class Movie
+{
+    public $title;
+    public $yearOfRelease;
+    public $rating;
+    public $nowInCinemas = false;
+
+    function __construct(string $title, int $yearOfRelease, int $rating) {
+        $this->title = $title;
+        $this->yearOfRelease = $yearOfRelease;
+        $this->rating = $rating;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+};
+
+
+?>
